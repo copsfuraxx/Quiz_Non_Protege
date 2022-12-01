f41cbd622fffb7be3a4684dc762f620ed0db43c1
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2022-12-02 00:21:53 +0100

    responsive game page

--- quiz/src/components/joueurs/index.jsx
+++ quiz/src/components/joueurs/index.jsx
@@ -4,23 +4,62 @@ function Joueur(){
         <div className="container-joueurs">
             <h3>Joueurs</h3>
             <div className="liste-joueurs">
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
-                <p className="joueur">Name</p>
+                <div className="container-joueur-score">
+                    <h4 className="joueur">Name</h4>
+                    <p>Score</p>
+                </div>
+                <div className="container-joueur-score">
+                    <h4 className="joueur">Name</h4>
+                    <p>Score</p>
+                </div>
+                <div className="container-joueur-score">
+                    <h4 className="joueur">Name</h4>
+                    <p>Score</p>
+                </div>
+                <div className="container-joueur-score">
+                    <h4 className="joueur">Name</h4>
+                    <p>Score</p>
+                </div>
+                <div className="container-joueur-score">
+                    <h4 className="joueur">Name</h4>
+                    <p>Score</p>
+                </div>
+                <div className="container-joueur-score">
+                    <h4 className="joueur">Name</h4>
+                    <p>Score</p>
+                </div>
+                <div className="container-joueur-score">
+                    <h4 className="joueur">Name</h4>
+                    <p>Score</p>
+                </div>
+                <div className="container-joueur-score">
+                    <h4 className="joueur">Name</h4>
+                    <p>Score</p>
+                </div>
+                <div className="container-joueur-score">
+                    <h4 className="joueur">Name</h4>
+                    <p>Score</p>
+                </div>
+                <div className="container-joueur-score">
+                    <h4 className="joueur">Name</h4>
+                    <p>Score</p>
+                </div>
+                <div className="container-joueur-score">
+                    <h4 className="joueur">Name</h4>
+                    <p>Score</p>
+                </div>
+                <div className="container-joueur-score">
+                    <h4 className="joueur">Name</h4>
+                    <p>Score</p>
+                </div>
+                <div className="container-joueur-score">
+                    <h4 className="joueur">Name</h4>
+                    <p>Score</p>
+                </div>
+                <div className="container-joueur-score">
+                    <h4 className="joueur">Name</h4>
+                    <p>Score</p>
+                </div>
             </div>
         </div>
     )

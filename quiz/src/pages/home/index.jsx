6517b2c7c90485144c7735c76a4e85661db8df28
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2022-12-01 23:51:29 +0100

    changement style reponse et liste joueur

--- quiz/src/pages/home/index.jsx
+++ quiz/src/pages/home/index.jsx
@@ -1,5 +1,4 @@
 import './styles/index.css';
-import Header from '../../components/header/index'
 import QuestionAnswer from '../../components/question-answer/index'
 import Joueurs from '../../components/joueurs/index'
 import Footer from '../../components/footer/index'
@@ -8,7 +7,6 @@ function Home()
 {
     return (
         <body>
-        <Header/>
             <div className="container-answer-question-joueurs">
                 <div className='container-question_answer'>
                     <QuestionAnswer/>

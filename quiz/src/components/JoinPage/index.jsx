dc1577b3040d1b3db3abd851c6bb8fedbd5dfd33
Corent12003 <119617629+Corent12003@users.noreply.github.com>
2022-12-02 00:47:59 +0100

    join couleur

--- quiz/src/components/JoinPage/index.jsx
+++ quiz/src/components/JoinPage/index.jsx
@@ -3,7 +3,7 @@ function JoinPage(){
     return(
         <div className='pin'>
             <input className='pininput' type="text" name='pin'/>
-            <input className='rejoindre' type="submit" name="connexion" value="S'inscrire" class="button"/>
+            <input className='join' type="submit" name="connexion" value="Rejoindre la salle" placeholder='Code Pin de la salle'/>
         </div>
     )
     

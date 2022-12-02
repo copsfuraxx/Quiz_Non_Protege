aa2b2c170055aa77c8a862cb82d85349ef835d9a
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2022-12-02 02:46:18 +0100

    fix bug

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,5 +1,7 @@
 import Home from './pages/home/index'
 import Game from './pages/game/index'
+// import CreateRoom from './pages/create-room/index'
+// import MyRoom from './pages/my-room/index'
 import {Routes,Route} from 'react-router-dom'
 import socketIO from 'socket.io-client'
 
@@ -10,8 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={ <Home socket={socket}/> } />
           <Route path="game" element={ <Game socket={socket}/> } />
-          <Route path="create-room" element={ <Create-room socket={socket}/> } />
-          <Route path="my-room" element={ <My-room socket={socket}/> } />
+          {/* <Route path="create-room" element={ <CreateRoom socket={socket}/> } />
+          <Route path="my-room" element={ <MyRoom socket={socket}/> } /> */}
         </Routes>
         </div>
         )

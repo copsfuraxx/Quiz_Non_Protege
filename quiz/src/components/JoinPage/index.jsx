7c5eabd1e324432bb1dfab11b54dfb25a0b179a9
romanautiste <mankind312.kub@gmail.com>
2022-12-02 02:40:15 +0100

    End of home page

--- quiz/src/components/JoinPage/index.jsx
+++ quiz/src/components/JoinPage/index.jsx
@@ -18,13 +18,14 @@ function JoinPage({socket}){
     return(
         <div>
             <div className='big-logo'>
-
             </div>
             {messages}
             <div className='pin'>
                 <input className='pin-input' type="text" name='pin' placeholder='Code Pin de la salle'/>
                 <button className='join' name="connexion">Rejoindre du Jeu</button>
-                <button className='join' name="connexion" onClick={() => hostGame()} >Creer une partie</button>
+            </div>
+            <div className='create-team'>
+                <a href="/create-room">Créez votre salle</a>
             </div>
             <div className='home-footer'>
                 <p>Copyright © Quizz non-protégé 2022</p>
